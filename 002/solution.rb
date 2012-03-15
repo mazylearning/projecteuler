0aaf116de1b49ec44b311c778ae20e57f035da7c
mazy learns <tepidtenacity@gmail.com>
2012-03-15 09:00:12 -0700

    Cleaned up syntax a bit

--- 002/solution.rb
+++ 002/solution.rb
@@ -1,12 +1,6 @@
-i = 1
-j = 2
-sum = 0
+i, j, sum = 1, 2, 0
 while j < 4000000 do
-    if j % 2 == 0
-        sum += j
-    end
-    k = i + j
-    i = j
-    j = k
+    sum += ( j % 2 == 0 ) ? j : 0
+    i, j = j, i + j
 end
 puts sum
